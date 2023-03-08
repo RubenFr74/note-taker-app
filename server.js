f5ee97a01e69ead5b49426378040221dd3c3a01e
Ruben Franco <ruben.afranco@yahoo.com>
2023-03-07 19:46:18 -0800

    added const for express

--- server.js
+++ server.js
@@ -2,3 +2,7 @@
 const express = require("express");
 const fs = require("fs");
 const path = require('path');
+
+// Starts express app
+const app = express();
+const PORT = process.env.PORT || 3000;
