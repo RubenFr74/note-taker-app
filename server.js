63aa37365687f38c8d6b015b1cf79ae7797e9d20
Ruben Franco <ruben.afranco@yahoo.com>
2023-03-07 19:53:27 -0800

    added data parsing

--- server.js
+++ server.js
@@ -6,3 +6,8 @@ const path = require('path');
 // Starts express app
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+// Setup data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(__dirname));
